65cb9779fd7b7d524472621bece55b973d881e0c
Akrem Benmbarek <akrem.benmbarek@ensi-uma.tn>
2023-06-27 18:44:11 +0100

    commit the part where the server sends a json file

---
+++ server.js
@@ -0,0 +1,31 @@
+const PORT = 8000
+const express = require('express')
+const cors = require('cors')
+const app = express ()
+app.use(express.json())
+app.use(cors())
+
+const API_KEY ='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'
+app.post('/completions',async(req,res) =>{
+    const options ={
+        method : "POST",
+        headers :{
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type" :"application/json"
+        },
+        body : JSON.stringify({
+            model : "gpt-3.5-turbo",
+            messages:[{role:"user",content:"how are you"}],
+            max_tokens:50,
+
+        })
+    }
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions',options)
+        const data = await response.json()
+        res.send(data)
+    }catch(error){
+        console.error(error)
+    }
+})
+app.listen(PORT,() => console.log("Your server is running on port :"+ PORT))
--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+const App = () => {
+
+  const getMessages= async() =>{
+    const Options ={
+      method:"POST",
+      body : JSON.stringify({
+        message:"hello how are you?"
+      }),
+      headers:{
+        "Content-Type":"application/json"
+      }
+    }
+    try{
+      const response = await fetch('http://localhost:8000/completions',Options)
+      const data =  await response.json()
+      
+      console.log(data)
+
+    }catch(error){
+      console.error(error)
+    }
+   
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className='side-bar'>
+        <button>
++ New Chat </button>
+          <ul className='history'>
+              <li>Blab</li>
+          </ul>
+          <nav>
+            <p>Made by AKrem</p>
+          </nav>
+      </section>
+      <section className='main'>
+        <h1>AKremGPT</h1>
+        <ul className='feed'>
+
+        </ul>
+        <div className='bottom-section'>
+          <div className='input-container'>
+            <input/>
+            <div id="submit" onClick={getMessages}>➢</div>
+            
+          </div>
+          <p className='info'>
+          Free Research Preview.
+           ChatGPT may produce inaccurate information about people, 
+           places, or facts. ChatGPT May 24 Version
+          </p>
+
+        </div>
+      </section>
+     
     </div>
   );
 }
